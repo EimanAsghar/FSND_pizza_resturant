848aa89f0a0645ed454919483ffcfb869f6c22ae
EimanAsghar <65137832+EimanAsghar@users.noreply.github.com>
2021-09-04 21:20:51 +0300

    add simple frontend

--- backend/src/app.py
+++ backend/src/app.py
@@ -3,6 +3,27 @@ from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
+#General Specifications:
+
+# Models will include at least:
+# 1- Two classes with primary keys at at least two attributes each
+# 2- [Optional but encouraged] One-to-many or many-to-many relationships between classes
+
+# Endpoints will include at least:
+# 1- Two GET requests
+# 2- One POST request
+# 3- One PATCH request
+# 4- One DELETE request
+
+# Roles will include at least:
+# 1- Two roles with different permissions
+# 2- Permissions specified for all endpoints
+
+# Tests will include at least:
+# 1- One test for success behavior of each endpoint
+# 2- One test for error behavior of each endpoint
+# 3- At least two tests of RBAC for each role
+
 def create_app(test_config=None):
   # create and configure the app
   app = Flask(__name__)
