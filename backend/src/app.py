fbacc5c578de34073e7f5c129c3cdde8cb0a0996
EimanAsghar <65137832+EimanAsghar@users.noreply.github.com>
2021-09-06 22:40:45 +0300

    update

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,6 +1,6 @@
 import os
 from flask import (
-  Flask, request, abort, jsonify, render_template
+    Flask, request, abort, jsonify, render_template
 )
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
@@ -10,7 +10,8 @@ from models import (
     order,
     pizza
 )
-#General Specifications:
+from datetime import datetime, timedelta
+# General Specifications:
 
 # Endpoints will include at least:
 # 1- Two GET requests
@@ -27,26 +28,123 @@ from models import (
 # 2- One test for error behavior of each endpoint
 # 3- At least two tests of RBAC for each role
 
+
 def create_app(test_config=None):
-  # create and configure the app
-  app = Flask(__name__, template_folder='templates')
-  setup_db(app)
+    # create and configure the app
+    app = Flask(__name__)
+    setup_db(app)
 
-  CORS(app, resources={r"/*": {"origins": "*"}})
+    CORS(app, resources={r"/*": {"origins": "*"}})
 
-  @app.after_request
-  def after_request(response):
+    @app.after_request
+    def after_request(response):
         response.headers.add('Access-Control-Allow-Headers',
                              'Content-Type, Authorization')
         response.headers.add('Access-Control-Allow-Methods',
                              'GET, POST, PATCH, DELETE, OPTIONS')
         return response
 
-  @app.route('/')
-  def index():
-    return render_template('index.html')
+    @app.route('/')
+    def index():
+        return render_template('index.html')
 
-  return app
+    # 1- Two GET requests
+    @app.route('/pizza')
+    def get_pizza():
+        pizza_result = pizza.query.all()
+        pizza_info = [p.information() for p in pizza_result]
+
+        return jsonify({
+            "success": True,
+            "pizza": pizza_info
+        }), 200
+
+    @app.route('/orders')
+    def get_orders():
+        order_result = order.query.all()
+        order_info = [o.information() for o in order_result]
+
+        return jsonify({
+            "success": True,
+            "order": order_info
+        }), 200
+
+    # 2- One POST request
+
+    # 3- One PATCH request
+    @app.route('/pizza/<int:pizza_id>', methods=['PATCH'])
+    def edit_pizza(pizza_id):
+
+        #get spacific pizza by id
+        pizza_result = pizza.query.filter(pizza.id == pizza_id).one_or_none()
+        body = request.get_json()
+
+        # send error if it not found
+        if pizza_result is None:
+            abort(404)
+        
+        if 'name' in body:
+            pizza_result.name = request.get_json().get('name')
+
+        elif 'price' in body:
+            pizza_result.price = request.get_json().get('price')
+
+        elif 'ingredients' in body:
+            pizza_result.ingredients = request.get_json().get('ingredients')
+
+
+        pizza_result.update()
+
+        return jsonify({
+            'success': True,
+            'pizza': [p.information() for p in pizza_result]
+        }), 200
+
+    # 4- One DELETE request
+
+    @app.route('/pizza/<int:pizza_id>', methods=['DELETE'])
+    def delete_pizza(pizza_id):
+        try:
+            # delete pizza based on spacific id
+            find_pizza = pizza.query.filter(
+                pizza.id == pizza_id).one_or_none()
+
+            find_pizza.delete()
+
+            return jsonify({
+                'success': True,
+                'deleted': pizza_id
+            }), 200
+
+        except:
+            abort(404)
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({
+            "success": False,
+            "error": 404,
+            "message": "not found"
+        }), 404
+
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return jsonify({
+            "success": False,
+            "error": 422,
+            "message": "unprocessable"
+        }), 422
+
+    @app.errorhandler(400)
+    def bad_request(error):
+        return jsonify({
+            "success": False,
+            "error": 400,
+            "message": "Bad Request"
+        }), 400
+
+    return app
+
 
 APP = create_app()
 
--- backend/src/models.py
+++ backend/src/models.py
@@ -6,14 +6,15 @@ import json
 
 from sqlalchemy.orm import backref
 from sqlalchemy.sql.expression import true
-
+from datetime import datetime
 
 # Models will include at least:
 # 1- Two classes with primary keys at at least two attributes each
 # 2- [Optional but encouraged] One-to-many or many-to-many relationships between classes
 
 database_name = "resturant"
-database_path = "postgresql://postgres:1998@{}/{}".format('localhost:5432', database_name)
+database_path = "postgresql://postgres:1998@{}/{}".format(
+    'localhost:5432', database_name)
 
 db = SQLAlchemy()
 
@@ -21,6 +22,8 @@ db = SQLAlchemy()
 setup_db(app)
     binds a flask application and a SQLAlchemy service
 '''
+
+
 def setup_db(app, database_path=database_path):
 
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
@@ -36,21 +39,68 @@ class user(db.Model):
     name = db.Column(db.String)
     address = db.Column(db.String(120))
     phone = db.Column(db.String(120))
-    
+
     order = db.relationship('order', backref='user', lazy=True)
+
+    def __init__(self, name, address, phone):
+        self.name = name
+        self.address = address
+        self.phone = phone
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def information(self):
+        return {
+            "name": self.name,
+            "address": self.address,
+            "phone": self.phone,
+        }
+
 
 class order(db.Model):
     id = db.Column(Integer, primary_key=True)
-    user_id = db.Column(Integer, db.ForeignKey('user.id'))
-    pizza_id = db.Column(Integer, db.ForeignKey('pizza.id')) 
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizza.id'), nullable=False)
     quantity = db.Column(db.Integer)
     order_time = db.Column(db.DateTime)
     pickup_time = db.Column(db.DateTime)
 
-    user = db.relationship('user', backref='order', lazy=True)
-    pizza = db.relationship('pizza', backref='order', lazy=True)
+    def __init__(self, user_id, pizza_id, quantity, order_time, pickup_time):
+        self.user_id = user_id
+        self.pizza_id = pizza_id
+        self.quantity = quantity
+        self.order_time = order_time
+        self.pickup_time = pickup_time
 
-    
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def information(self):
+        return {
+            "user_id": self.user_id,
+            "pizza_id": self.pizza_id,
+            "quantity": self.quantity,
+            "order_time": self.order_time.strftime("%m/%d/%Y, %H:%M:%S"),
+            "pickup_time": self.pickup_time.strftime("%m/%d/%Y, %H:%M:%S")
+        }
+
 
 class pizza(db.Model):
     id = db.Column(Integer, primary_key=True)
@@ -59,3 +109,27 @@ class pizza(db.Model):
     ingredients = db.Column(db.String(500))
 
     order = db.relationship('order', backref='pizza', lazy=True)
+
+    def __init__(self, name, price, ingredients):
+        self.name = name
+        self.price = price
+        self.ingredients = ingredients
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def information(self):
+        return {
+            "id":self.id,
+            "name": self.name,
+            "price": self.price,
+            "ingredients": self.ingredients
+        }
