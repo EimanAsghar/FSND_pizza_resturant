53711f3656f30522208e6a93ecf25d14796138e1
EimanAsghar <65137832+EimanAsghar@users.noreply.github.com>
2021-09-10 04:43:54 +0300

    remove comment

--- test_app.py
+++ test_app.py
@@ -35,159 +35,159 @@ class PizzaResturantTestCase(unittest.TestCase):
         """Executed after reach test"""
         pass
 
-    # Success for all pizza
-    # def test_get_all_pizza(self):
-    #     res = self.client().get('/pizza')
-    #     data = json.loads(res.data)
-    #     # check the status code
-    #     self.assertEqual(res.status_code, 200)
-    #     self.assertEqual(data['success'], True)
+    #Success for all pizza
+    def test_get_all_pizza(self):
+        res = self.client().get('/pizza')
+        data = json.loads(res.data)
+        # check the status code
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
 
-#     # Success for all order
-#     def test_get_all_orders(self):
-#         res = self.client().get('/orders')
-#         data = json.loads(res.data)
-#         # check the status code
-#         self.assertEqual(res.status_code, 200)
-#         self.assertEqual(data['success'], True)
+    # Success for all order
+    def test_get_all_orders(self):
+        res = self.client().get('/orders')
+        data = json.loads(res.data)
+        # check the status code
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
 
 
-#     # Success for order of spacific customer
-#     def test_user_exist(self):
-#         res = self.client().get('/orders/1')
+    # Success for order of spacific customer
+    def test_user_exist(self):
+        res = self.client().get('/orders/1')
 
-#         data = json.loads(res.data)
-#         # check if the return values equal to these.
-#         self.assertEqual(res.status_code, 200)
-#         self.assertEqual(data['success'], True)
+        data = json.loads(res.data)
+        # check if the return values equal to these.
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
 
     
-#     # error for order of spacific customer
-#     def test_user_does_not_exist(self):
-#         res = self.client().get('/orders/1000')
+    # error for order of spacific customer
+    def test_user_does_not_exist(self):
+        res = self.client().get('/orders/1000')
 
-#         data = json.loads(res.data)
-#         # check if the return values equal to these.
-#         self.assertEqual(res.status_code, 404)
-#         self.assertEqual(data['success'], False)
-#         self.assertEqual(data['message'], 'not found')
+        data = json.loads(res.data)
+        # check if the return values equal to these.
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'not found')
 
-#     # Success for create account
-#     def test_create_new_account(self):
-#         res = self.client().post('/users', json={
-#             'name': 'Sara',
-#             'email':  'sara@gmail.com',
-#             'address': 'albsaten',
-#             'phone': '0548673985'
-#         })
+    # Success for create account
+    def test_create_new_account(self):
+        res = self.client().post('/users', json={
+            'name': 'Sara',
+            'email':  'sara@gmail.com',
+            'address': 'albsaten',
+            'phone': '0548673985'
+        })
 
-#         data = json.loads(res.data)
-#         # check the status code
-#         self.assertEqual(res.status_code, 200)
+        data = json.loads(res.data)
+        # check the status code
+        self.assertEqual(res.status_code, 200)
 
-#     # error for create account
-#     def test_create_account_not_allowed(self):
+    # error for create account
+    def test_create_account_not_allowed(self):
 
-#         # send bad request
-#         res = self.client().post('/users', json={})
+        # send bad request
+        res = self.client().post('/users', json={})
 
-#         data = json.loads(res.data)
-#         # check the status code
-#         self.assertEqual(res.status_code, 422)
-#         self.assertEqual(data['success'], False)
-#         self.assertEqual(data['message'], 'unprocessable')
-
-
-#     # Success for add new pizza 
-#     def test_add_new_pizza(self):
-#         res = self.client().post('/pizza', json={
-#             'name': 'Pepperoni',
-#             'price':  '45',
-#             'ingredients': 'Beef Pepperoni, Mozzarella & Signature Tomato Sauce',
-#         })
-
-#         data = json.loads(res.data)
-#         # check the status code
-#         self.assertEqual(res.status_code, 200)
-
-#     # error for add new pizza 
-#     def test_add_new_pizza_not_allowed(self):
-
-#         # send bad request
-#         res = self.client().post('/pizza', json={})
-
-#         data = json.loads(res.data)
-#         # check the status code
-#         self.assertEqual(res.status_code, 422)
-#         self.assertEqual(data['success'], False)
-#         self.assertEqual(data['message'], 'unprocessable')
+        data = json.loads(res.data)
+        # check the status code
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'unprocessable')
 
 
+    # Success for add new pizza 
+    def test_add_new_pizza(self):
+        res = self.client().post('/pizza', json={
+            'name': 'Pepperoni',
+            'price':  '45',
+            'ingredients': 'Beef Pepperoni, Mozzarella & Signature Tomato Sauce',
+        })
 
-#  # Success for make new order
-#     def test_make_new_order(self):
-#         res = self.client().post('/orders/1/create', json={
-#             'pizza_id': '1',
-#             'quantity':  '4'
-#         })
+        data = json.loads(res.data)
+        # check the status code
+        self.assertEqual(res.status_code, 200)
 
-#         data = json.loads(res.data)
-#         # check the status code
-#         self.assertEqual(res.status_code, 200)
+    # error for add new pizza 
+    def test_add_new_pizza_not_allowed(self):
 
-#     # error for add new pizza 
-#     def test_make_new_order_not_allowed(self):
+        # send bad request
+        res = self.client().post('/pizza', json={})
 
-#         # send bad request
-#         res = self.client().post('/orders/1/create', json={})
-
-#         data = json.loads(res.data)
-#         # check the status code
-#         self.assertEqual(res.status_code, 422)
-#         self.assertEqual(data['success'], False)
-#         self.assertEqual(data['message'], 'unprocessable')
-
-#     #Success for edit exist pizza
-#     def test_edit_exist_pizza(self):
-#         res = self.client().patch('/pizza/1')
-
-#         data = json.loads(res.data)
-#         # check if the return values equal to these.
-#         self.assertEqual(res.status_code, 200)
-#         self.assertEqual(data['success'], True)
-
-#     #error for edit exist pizza
-#     def test_edit_pizza_not_exist(self):
-#         res = self.client().patch('/pizza/1000')
-
-#         data = json.loads(res.data)
-#         # check if the return values equal to these.
-#         self.assertEqual(res.status_code, 404)
-#         self.assertEqual(data['success'], False)
-#         self.assertEqual(data['message'], 'not found')
+        data = json.loads(res.data)
+        # check the status code
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'unprocessable')
 
 
-#     # Success for delete pizza
-#     def test_delete_pizza(self):
-#         res = self.client().delete('/pizza/1')
 
-#         data = json.loads(res.data)
-#         self.assertEqual(res.status_code, 200)
-#         # check if the return id equla to 1
-#         self.assertEqual(data['deleted'], 1)
-#         # find the pizza by id to check if it's deleted or not
-#         self.assertEqual(pizza.query.filter(
-#             pizza.id == 1).one_or_none(), None)
+ # Success for make new order
+    def test_make_new_order(self):
+        res = self.client().post('/orders/1/create', json={
+            'pizza_id': '1',
+            'quantity':  '4'
+        })
 
-#     # error for delete pizza
-#     def test_pizza_does_not_exist(self):
-#         res = self.client().delete('/pizza/1000')
+        data = json.loads(res.data)
+        # check the status code
+        self.assertEqual(res.status_code, 200)
 
-#         data = json.loads(res.data)
-#         # check if the return values equal to these.
-#         self.assertEqual(res.status_code, 404)
-#         self.assertEqual(data['success'], False)
-#         self.assertEqual(data['message'], 'not found')
+    # error for add new pizza 
+    def test_make_new_order_not_allowed(self):
+
+        # send bad request
+        res = self.client().post('/orders/1/create', json={})
+
+        data = json.loads(res.data)
+        # check the status code
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'unprocessable')
+
+    #Success for edit exist pizza
+    def test_edit_exist_pizza(self):
+        res = self.client().patch('/pizza/1')
+
+        data = json.loads(res.data)
+        # check if the return values equal to these.
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+
+    #error for edit exist pizza
+    def test_edit_pizza_not_exist(self):
+        res = self.client().patch('/pizza/1000')
+
+        data = json.loads(res.data)
+        # check if the return values equal to these.
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'not found')
+
+
+    # Success for delete pizza
+    def test_delete_pizza(self):
+        res = self.client().delete('/pizza/1')
+
+        data = json.loads(res.data)
+        self.assertEqual(res.status_code, 200)
+        # check if the return id equla to 1
+        self.assertEqual(data['deleted'], 1)
+        # find the pizza by id to check if it's deleted or not
+        self.assertEqual(pizza.query.filter(
+            pizza.id == 1).one_or_none(), None)
+
+    # error for delete pizza
+    def test_pizza_does_not_exist(self):
+        res = self.client().delete('/pizza/1000')
+
+        data = json.loads(res.data)
+        # check if the return values equal to these.
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'not found')
 
 
 
